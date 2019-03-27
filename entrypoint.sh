a17b5ce55d7a2eafef471171da4519d1ce5f2fe7
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-03-27 17:07:55 +0530

    Add missing secret error handling
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# Check required env variables
+flag=0
+if [[ -z "$SSH_PRIVATE_KEY" ]]; then
+    flag=1
+    missing_secret="SSH_PRIVATE_KEY"
+    if [[ -n "$VAULT_ADDR" ]] && [[ -n "$VAULT_TOKEN" ]]; then
+        flag=0
+    fi
+    if [[ -n "$VAULT_ADDR" ]] || [[ -n "$VAULT_TOKEN" ]]; then
+        missing_secret="VAULT_ADDR and/or VAULT_TOKEN"
+    fi
+fi
+
+if [[ "$flag" -eq 1 ]]; then
+    printf "[\e[0;31mERROR\e[0m] Secret \`$missing_secret\` is missing. Please add it to this action for proper execution.\nRefer https://github.com/rtCamp/action-deploy-wordpress for more information.\n"
+    exit 1
+fi
+
 # custom path for files to override default files
 custom_path="$GITHUB_WORKSPACE/.github/deploy"
 main_script="/main.sh"
