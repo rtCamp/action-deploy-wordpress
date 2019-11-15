5d4efc0d3df7d8e70f7fd4de3cd6964d27ef0958
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2019-11-15 15:10:26 +0530

    Fix syntax error

--- main.sh
+++ main.sh
@@ -108,7 +108,7 @@ wp core download --version="$WP_VERSION" --allow-root
 rm -r wp-content/
 
 # Include webroot-files in htdocs if they exists
-if [[ -d "$GITHUB_WORKSPACE/webroot-files" ]]; then;
+if [[ -d "$GITHUB_WORKSPACE/webroot-files" ]]; then
     rsync -av  "$GITHUB_WORKSPACE/webroot-files/" "$HTDOCS/"  > /dev/null
     rm -rf "$GITHUB_WORKSPACE/webroot-files/"
 fi
