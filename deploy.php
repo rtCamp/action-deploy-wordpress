312a8837da2913698d9fa4cd0853808bde81d492
Riddhesh Sanghvi <riddhesh237@gmail.com>
2022-02-24 10:28:47 +0530

    Update cachetool download to autodetect PHP version
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- deploy.php
+++ deploy.php
@@ -72,8 +72,32 @@ set( 'rsync_dest', '{{release_path}}' );
 desc( 'Download cachetool' );
 task( 'cachetool:download', function () {
 
-	# Using 5.x for PHP >=7.2 compatibility
-	run( 'wget https://github.com/gordalina/cachetool/releases/download/5.1.3/cachetool.phar -O {{release_path}}/cachetool.phar' );
+	$php_version = getenv( 'PHP_VERSION' );
+	if ( empty( $php_version ) ) {
+		$ee_version = '';
+		try {
+			$ee_version = run( 'ee --version' );
+		} catch ( \Exception $e ) {
+			echo 'Not using EasyEngine.';
+		}
+
+		if ( false !== strpos( $ee_version, 'EE 4' ) ) {
+			try {
+				$php_version = run( 'cd {{deploy_path}} && ee shell --command="php -r \'echo PHP_MAJOR_VERSION;\'" --skip-tty' );
+			} catch ( \Exception $e ) {
+				echo 'Could not determine PHP version. Use `PHP_VERSION` env variable to specify the version.';
+				echo 'Falling back to version 7.4 as default';
+				$php_version = 7.4;
+			}
+		}
+	}
+
+	if ( $php_version < 8 ) {
+		# Using 5.x for PHP >=7.2 compatibility
+		run( 'wget https://github.com/gordalina/cachetool/releases/download/5.1.3/cachetool.phar -O {{release_path}}/cachetool.phar' );
+	} else {
+		run( 'wget https://github.com/gordalina/cachetool/releases/download/8.4.0/cachetool.phar -O {{release_path}}/cachetool.phar' );
+	}
 } );
 
 /*  custom task defination    */
