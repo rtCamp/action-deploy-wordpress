6e6bef42f7dab29832fc1342d177af030001df25
Riddhesh Sanghvi <riddhesh237@gmail.com>
2022-02-24 09:33:48 +0530

    Format shell script
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,19 @@
 # Check required env variables
 flag=0
 if [[ -z "$SSH_PRIVATE_KEY" ]]; then
-    flag=1
-    missing_secret="SSH_PRIVATE_KEY"
-    if [[ -n "$VAULT_ADDR" ]] && [[ -n "$VAULT_TOKEN" ]]; then
-        flag=0
-    fi
-    if [[ -n "$VAULT_ADDR" ]] || [[ -n "$VAULT_TOKEN" ]]; then
-        missing_secret="VAULT_ADDR and/or VAULT_TOKEN"
-    fi
+	flag=1
+	missing_secret="SSH_PRIVATE_KEY"
+	if [[ -n "$VAULT_ADDR" ]] && [[ -n "$VAULT_TOKEN" ]]; then
+		flag=0
+	fi
+	if [[ -n "$VAULT_ADDR" ]] || [[ -n "$VAULT_TOKEN" ]]; then
+		missing_secret="VAULT_ADDR and/or VAULT_TOKEN"
+	fi
 fi
 
 if [[ "$flag" -eq 1 ]]; then
-    printf "[\e[0;31mERROR\e[0m] Secret \`$missing_secret\` is missing. Please add it to this action for proper execution.\nRefer https://github.com/rtCamp/action-deploy-wordpress for more information.\n"
-    exit 1
+	printf "[\e[0;31mERROR\e[0m] Secret \`$missing_secret\` is missing. Please add it to this action for proper execution.\nRefer https://github.com/rtCamp/action-deploy-wordpress for more information.\n"
+	exit 1
 fi
 
 # custom path for files to override default files
@@ -23,8 +23,8 @@ custom_path="$GITHUB_WORKSPACE/.github/deploy"
 main_script="/main.sh"
 
 if [[ -d "$custom_path" ]]; then
-    rsync -av "$custom_path/" /
-    chmod +x /*.sh
+	rsync -av "$custom_path/" /
+	chmod +x /*.sh
 fi
 
 bash "$main_script"
--- main.sh
+++ main.sh
@@ -7,7 +7,6 @@ export HTDOCS="$HOME/htdocs"
 export GITHUB_BRANCH=${GITHUB_REF##*heads/}
 export CI_SCRIPT_OPTIONS="ci_script_options"
 
-
 function init_checks() {
 
 	# Check if branch is available
@@ -18,7 +17,7 @@ function init_checks() {
 
 	# Check for SSH key if jump host is defined
 	if [[ ! -z "$JUMPHOST_SERVER" ]]; then
-		
+
 		if [[ -z "$SSH_PRIVATE_KEY" ]]; then
 			echo "Jump host configuration does not work with vault ssh signing."
 			echo "SSH_PRIVATE_KEY secret needs to be added."
@@ -45,9 +44,9 @@ function check_branch_in_hosts_file() {
 
 	match=0
 	for branch in $(cat "$hosts_file" | shyaml keys); do
-		[[ "$GITHUB_REF" = "refs/heads/$branch" ]] && \
-		echo "$GITHUB_REF matches refs/heads/$branch" && \
-		match=1
+		[[ "$GITHUB_REF" = "refs/heads/$branch" ]] &&
+			echo "$GITHUB_REF matches refs/heads/$branch" &&
+			match=1
 	done
 
 	# Exit neutral if no match found
@@ -60,14 +59,14 @@ function check_branch_in_hosts_file() {
 function setup_private_key() {
 
 	if [[ -n "$SSH_PRIVATE_KEY" ]]; then
-	echo "$SSH_PRIVATE_KEY" | tr -d '\r' > "$SSH_DIR/id_rsa"
-	chmod 600 "$SSH_DIR/id_rsa"
-	eval "$(ssh-agent -s)"
-	ssh-add "$SSH_DIR/id_rsa"
+		echo "$SSH_PRIVATE_KEY" | tr -d '\r' >"$SSH_DIR/id_rsa"
+		chmod 600 "$SSH_DIR/id_rsa"
+		eval "$(ssh-agent -s)"
+		ssh-add "$SSH_DIR/id_rsa"
 
-	if [[ -n "$JUMPHOST_SERVER" ]]; then
-		ssh-keyscan -H "$JUMPHOST_SERVER" >> /etc/ssh/known_hosts 
-	fi
+		if [[ -n "$JUMPHOST_SERVER" ]]; then
+			ssh-keyscan -H "$JUMPHOST_SERVER" >>/etc/ssh/known_hosts
+		fi
 	else
 		# Generate a key-pair
 		ssh-keygen -t rsa -b 4096 -C "GH-actions-ssh-deploy-key" -f "$HOME/.ssh/id_rsa" -N ""
@@ -79,28 +78,28 @@ function maybe_get_ssh_cert_from_vault() {
 	# Get signed key from vault
 	if [[ -n "$VAULT_GITHUB_TOKEN" ]]; then
 		unset VAULT_TOKEN
-		vault login -method=github token="$VAULT_GITHUB_TOKEN" > /dev/null
+		vault login -method=github token="$VAULT_GITHUB_TOKEN" >/dev/null
 	fi
 
 	if [[ -n "$VAULT_ADDR" ]]; then
-		vault write -field=signed_key ssh-client-signer/sign/my-role public_key=@$HOME/.ssh/id_rsa.pub > $HOME/.ssh/signed-cert.pub
+		vault write -field=signed_key ssh-client-signer/sign/my-role public_key=@$HOME/.ssh/id_rsa.pub >$HOME/.ssh/signed-cert.pub
 	fi
 }
 
 function configure_ssh_config() {
 
-if [[ -z "$JUMPHOST_SERVER" ]]; then
-	# Create ssh config file. `~/.ssh/config` does not work.
-	cat > /etc/ssh/ssh_config <<EOL
+	if [[ -z "$JUMPHOST_SERVER" ]]; then
+		# Create ssh config file. `~/.ssh/config` does not work.
+		cat >/etc/ssh/ssh_config <<EOL
 Host $hostname
 HostName $hostname
 IdentityFile ${SSH_DIR}/signed-cert.pub
 IdentityFile ${SSH_DIR}/id_rsa
 User $ssh_user
 EOL
-else
-	# Create ssh config file. `~/.ssh/config` does not work.
-	cat > /etc/ssh/ssh_config <<EOL
+	else
+		# Create ssh config file. `~/.ssh/config` does not work.
+		cat >/etc/ssh/ssh_config <<EOL
 Host jumphost
 	HostName $JUMPHOST_SERVER
 	UserKnownHostsFile /etc/ssh/known_hosts
@@ -112,7 +111,7 @@ Host $hostname
 	UserKnownHostsFile /etc/ssh/known_hosts
 	User $ssh_user
 EOL
-fi
+	fi
 
 }
 
@@ -138,26 +137,26 @@ function maybe_install_submodules() {
 	# Check and update submodules if any
 	if [[ -f "$GITHUB_WORKSPACE/.gitmodules" ]]; then
 		# add github's public key
-		echo "|1|qPmmP7LVZ7Qbpk7AylmkfR0FApQ=|WUy1WS3F4qcr3R5Sc728778goPw= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> /etc/ssh/known_hosts
+		echo "|1|qPmmP7LVZ7Qbpk7AylmkfR0FApQ=|WUy1WS3F4qcr3R5Sc728778goPw= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >>/etc/ssh/known_hosts
 
 		identity_file=''
 		if [[ -n "$SUBMODULE_DEPLOY_KEY" ]]; then
-			echo "$SUBMODULE_DEPLOY_KEY" | tr -d '\r' > "$SSH_DIR/submodule_deploy_key"
+			echo "$SUBMODULE_DEPLOY_KEY" | tr -d '\r' >"$SSH_DIR/submodule_deploy_key"
 			chmod 600 "$SSH_DIR/submodule_deploy_key"
 			ssh-add "$SSH_DIR/submodule_deploy_key"
 			identity_file="IdentityFile ${SSH_DIR}/submodule_deploy_key"
 		fi
 
-	# Setup config file for proper git cloning
-	cat >> /etc/ssh/ssh_config <<EOL
+		# Setup config file for proper git cloning
+		cat >>/etc/ssh/ssh_config <<EOL
 Host github.com
 HostName github.com
 User git
 UserKnownHostsFile /etc/ssh/known_hosts
 ${identity_file}
 EOL
-	git submodule update --init --recursive
-fi
+		git submodule update --init --recursive
+	fi
 }
 
 function setup_wordpress_files() {
@@ -166,7 +165,7 @@ function setup_wordpress_files() {
 	cd "$HTDOCS"
 	export build_root="$(pwd)"
 
-	hosts_wp_version=$(cat "$hosts_file" | shyaml get-value "$GITHUB_BRANCH.WP_VERSION" 2> /dev/null)
+	hosts_wp_version=$(cat "$hosts_file" | shyaml get-value "$GITHUB_BRANCH.WP_VERSION" 2>/dev/null)
 
 	# Check if WP_VERSION is already defined in hosts.yml
 	# Priority: 1. hosts.yml, 2. workflow file, else use latest
@@ -177,9 +176,9 @@ function setup_wordpress_files() {
 	fi
 
 	if [[ "$WP_MINOR_UPDATE" == "true" ]] && [[ "$WP_VERSION" != "latest" ]]; then
-		LATEST_MINOR_VERSION=$(\
-			curl -s "https://api.wordpress.org/core/version-check/1.7/?version=$WP_VERSION" | \
-			jq -r '[.offers[]|select(.response=="autoupdate")][-1].version'
+		LATEST_MINOR_VERSION=$(
+			curl -s "https://api.wordpress.org/core/version-check/1.7/?version=$WP_VERSION" |
+				jq -r '[.offers[]|select(.response=="autoupdate")][-1].version'
 		)
 		MAJOR_DOT_MINOR=$(echo "$WP_VERSION" | cut -c1-3)
 		if [[ "$LATEST_MINOR_VERSION" == "$MAJOR_DOT_MINOR"* ]]; then
@@ -196,11 +195,11 @@ function setup_wordpress_files() {
 
 	# Include webroot-files in htdocs if they exists
 	if [[ -d "$GITHUB_WORKSPACE/webroot-files" ]]; then
-		rsync -av  "$GITHUB_WORKSPACE/webroot-files/" "$HTDOCS/"  > /dev/null
+		rsync -av "$GITHUB_WORKSPACE/webroot-files/" "$HTDOCS/" >/dev/null
 		rm -rf "$GITHUB_WORKSPACE/webroot-files/"
 	fi
 
-	rsync -av  "$GITHUB_WORKSPACE/" "$HTDOCS/wp-content/"  > /dev/null
+	rsync -av "$GITHUB_WORKSPACE/" "$HTDOCS/wp-content/" >/dev/null
 
 	# Remove uploads directory
 	cd "$HTDOCS/wp-content/"
